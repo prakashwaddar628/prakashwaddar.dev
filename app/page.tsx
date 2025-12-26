58f71e82687924a9d388fc61f0c774abea78a461
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-12-26 09:09:53 +0530

    navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
+import Navbar from "@/components/navbar/Navbar";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center
-     justify-center bg-zinc-50 font-sans dark:bg-black">
-      <nav className="text-white flex justify-between">
-        <Link href="/writing" className="mx-4">Writing</Link>
-        <Link href="/logs">Logs</Link>
-      </nav>
+    <div className="flex min-h-screen">
+      <Navbar />
     </div>
   );
 }
